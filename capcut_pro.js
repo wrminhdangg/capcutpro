b41ae1b4c1b934e5374735d2b01724fbe19aeb91
wrminhdangg <nguyendang08781@gmail.com>
2025-03-23 22:55:52 +0700

    Update capcut_pro.js

--- capcut_pro.js
+++ capcut_pro.js
@@ -4,7 +4,20 @@ const response = {
     "data": {
         "is_pro": true,
         "subscription_expiry": "2099-12-31T23:59:59Z",
-        "features": ["no_ads", "4k_export", "premium_assets", "remove_watermark"]
+        "features": [
+            "no_ads", 
+            "4k_export", 
+            "premium_assets", 
+            "remove_watermark", 
+            "unlock_templates", 
+            "unlock_filters", 
+            "vip_effects", 
+            "hd_export", 
+            "pro_stickers", 
+            "ai_background_removal", 
+            "premium_fonts", 
+            "music_library_access"
+        ]
     }
 };
 
